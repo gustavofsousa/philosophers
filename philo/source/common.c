5fb10ac40814a5793388487b4d7bddc61bdb64a2
gusousa <gfsousa42@gmail.com>
2022-12-23 16:33:36 -0300

    Add diferents collors to printf

--- philo/source/common.c
+++ philo/source/common.c
@@ -1,14 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   common.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gusousa <gusousa@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/23 11:03:45 by gusousa           #+#    #+#             */
+/*   Updated: 2022/12/23 16:33:04 by gusousa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 /*
-*  ** Returns the timestamp in milliseconds.
-*   */
+ * Get the actual time and return it.
+ * The function returns the precision of time in seconds + microseconds(10^-6)
+ * We convert both values to milliseconds(10^-3).
+*/
 long	get_time(void)
 {
-	struct timeval	tp;
+	struct timeval	tv;
 	long	milliseconds;
 
-	gettimeofday(&tp, NULL); //what is second argument?
-	milliseconds = tp.tv_sec * 1000;
-	milliseconds += tp.tv_usec / 1000;
+	gettimeofday(&tv, NULL);
+	milliseconds = tv.tv_sec * 1000;
+	milliseconds += tv.tv_usec / 1000;
 	return (milliseconds);
 }
 
@@ -17,4 +31,39 @@ int	throw_error(char *error)
 	printf("\033[0;31m\n");// Colocar em um e_num ou macro?
 	printf("Error\n%s\n", error);
 	printf("\033[0ms");
+}
+
+void	print_red(char *str)
+{
+	printf("\033[1;31m");
+	printf("%s", str);
+	printf("\033[0m");
+}
+
+void	print_yellow(char *str)
+{
+	printf("\033[0;33m");
+	printf("%s", str);
+	printf("\033[0m");
+}
+
+void	print_cyan(char *str)
+{
+	printf("\033[0;36m");
+	printf("%s", str);
+	printf("\033[0m");
+}
+
+void	print_white(char *str)
+{
+	printf("\033[0;37m");
+	printf("%s", str);
+	printf("\033[0m");
+}
+
+void	print_green(char *str)
+{
+	printf("\033[0;32m");
+	printf("%s", str);
+	printf("\033[0m");
 }
