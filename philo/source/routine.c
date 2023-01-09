b36e170a9900c11bf1509211e96a4aec25194327
gusousa <gfsousa42@gmail.com>
2023-01-09 12:08:16 -0300

    begin of routine

--- philo/source/routine.c
+++ philo/source/routine.c
@@ -6,8 +6,22 @@
 /*   By: gusousa <gusousa@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 14:59:51 by gusousa           #+#    #+#             */
-/*   Updated: 2022/12/22 15:00:08 by gusousa          ###   ########.fr       */
+/*   Updated: 2023/01/09 12:07:55 by gusousa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
+
+void	*routine(void *data)
+{
+
+	time_of_last_meal = get_time();
+	while (data->all_philos->dead)
+	{
+		taking_hashi(philo);
+		eating(philo);
+		sleeping(philo);
+		thinking(philo);
+	}
+	return (NULL);
+}
