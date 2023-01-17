2804213b01f1c4489f0302968b13fdc393cd5c38
gusousa <gfsousa42@gmail.com>
2023-01-17 15:41:23 -0300

    Fix: \n end print_usage

--- philo/source/print_color.c
+++ philo/source/print_color.c
@@ -6,7 +6,7 @@
 /*   By: gusousa <gusousa@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 11:03:45 by gusousa           #+#    #+#             */
-/*   Updated: 2023/01/17 13:35:58 by gusousa          ###   ########.fr       */
+/*   Updated: 2023/01/17 15:40:37 by gusousa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,15 +46,15 @@ int	print_green(char *str)
 
 int	print_usage(void)
 {
-	print_red(" ________________________________________________ \n");
-	print_red("|          Please enter 4 or 5 arguments         |\n");
-	print_red("|           (only non-signal intergers)          |\n");
-	print_red("|________________________________________________|\n");
-	print_red("|           [1][Number of philosophers]          |\n");
-	print_red("|           [2][Time to die]                     |\n");
-	print_red("|           [3][Time to eat]                     |\n");
-	print_red("|           [4][Time to sleep]                   |\n");
-	print_red("|           [5][Number of meals]                 |\n");
-	print_red("|________________________________________________|\n");
+	print_red(" ________________________________________________ ");
+	print_red("|          Please enter 4 or 5 arguments         |");
+	print_red("|           (only non-signal intergers)          |");
+	print_red("|________________________________________________|");
+	print_red("|           [1][Number of philosophers]          |");
+	print_red("|           [2][Time to die]                     |");
+	print_red("|           [3][Time to eat]                     |");
+	print_red("|           [4][Time to sleep]                   |");
+	print_red("|           [5][Number of meals]                 |");
+	print_red("|________________________________________________|");
 	return (1);
 }
