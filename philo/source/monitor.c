42dd9a27178c22636fe24d93b8bd0c55e51ed52e
gusousa <gfsousa42@gmail.com>
2023-01-19 15:45:08 -0300

    add usleep in monitor

--- philo/source/monitor.c
+++ philo/source/monitor.c
@@ -6,7 +6,7 @@
 /*   By: gusousa <gusousa@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 14:59:51 by gusousa           #+#    #+#             */
-/*   Updated: 2023/01/19 11:02:30 by gustavosousa     ###   ########.fr       */
+/*   Updated: 2023/01/19 15:43:52 by gusousa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,7 @@ void	*monitoring(void *args)
 		if (time_since_lm == data->time_to_die)
 			return (hit_and_run(data, i));
 		pthread_mutex_unlock(&data->lock_print);
+		usleep(200);
 	}
 	return (NULL);
 }
